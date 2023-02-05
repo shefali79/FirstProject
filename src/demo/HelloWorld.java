20ac4a571fee7892a0ac5a33ccdc0c0770b600a2
shefali islam <islamshefali79@gmail.com>
2023-02-05 13:19:04 -0500

    This is my hello world program

---
+++ src/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Shefali Islam");
+		System.out.println("New York");
+		System.out.println("Shefali Islam");
+
+		
+
+	}
+
+}
